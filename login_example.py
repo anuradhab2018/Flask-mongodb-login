09a2ad975dc91764bf107d425a36c969454e73d6
Anuradha B <64590128+anuradhab2018@users.noreply.github.com>
2020-09-24 15:06:12 +0530

    Add files via upload

---
+++ login_example.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, url_for, request, session, redirect,flash
+from flask_pymongo import PyMongo
+import bcrypt
+from PIL import Image
+from captcha.image import ImageCaptcha
+from flask_recaptcha import ReCaptcha
+
+app = Flask(__name__)
+recaptcha = ReCaptcha(app=app)
+
+app.config.update(dict(
+    RECAPTCHA_ENABLED=True,
+    RECAPTCHA_SITE_KEY="6LeKOaMZAAAAAI7L6TVsZa9A2t6-9LDVYSVqX9ZP",
+    RECAPTCHA_SECRET_KEY="6LeKOaMZAAAAAKw9nhAjnpzrzrC3R0YYRf-kKDH1",
+))
+recaptcha = ReCaptcha()
+recaptcha.init_app(app)
+app.config['MONGO_DBNAME'] = 'mongologinexample'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mongologinexample'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user["password"]:
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+        
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'],'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(debug=True)
